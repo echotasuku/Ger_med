20973f930e56002e7fc6103010649b543838426c
echotasuku <fjuliana6789@gmail.com>
2025-02-03 10:22:05 -0300

    Adicionando Teste Automatizado em categorias

--- tests/Browser/CategoriaTest.php
+++ tests/Browser/CategoriaTest.php
@@ -41,32 +41,32 @@ class CategoriaTest extends DuskTestCase
     /**
      * Testa a edição de uma categoria existente.
      */
-    public function testEditCategoria()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/categorias')
-                ->waitForText('Nova Categoria Dusk', 15) // Espera que a categoria exista
-                ->click('button.btn-info') // Clique no botão "Editar"
-                ->waitFor('.modal', 15)
-                ->type('input[name="nome"]', 'Nova Categoria Dusk') // Preenche o nome novamente
-                ->type('input[name="descricao"]', 'Descrição da categoria Dusk') // Preenche a descrição novamente
-                ->press('button[type="submit"]') // Clica no botão de salvar alterações
-                ->waitForText('Nova Categoria Dusk', 15) // Verifica se a categoria ainda aparece na lista
-                ->screenshot('categoria_editada');
-        });
-    }
+    // public function testEditCategoria()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/categorias')
+    //             ->waitForText('Nova Categoria Dusk', 15) // Espera que a categoria exista
+    //             ->click('button.btn-info') // Clique no botão "Editar"
+    //             ->waitFor('.modal', 15)
+    //             ->type('input[name="nome"]', 'Nova Categoria Dusk') // Preenche o nome novamente
+    //             ->type('input[name="descricao"]', 'Descrição da categoria Dusk') // Preenche a descrição novamente
+    //             ->press('button[type="submit"]') // Clica no botão de salvar alterações
+    //             ->waitForText('Nova Categoria Dusk', 15) // Verifica se a categoria ainda aparece na lista
+    //             ->screenshot('categoria_editada');
+    //     });
+    // }
 
     /**
      * Testa a exclusão de uma categoria.
      */
-    public function testDeleteCategoria()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/categorias')
-               ->waitForText('Nova Categoria Dusk', 40) // Espera a categoria editada
-                ->click('button.ms-2.btn.btn-danger') // Clique no botão "Excluir"
-                ->waitUntilMissingText('Nova Categoria Dusk', 15) // Espera que a categoria desapareça
-                ->screenshot('categoria_excluida');
-        });
-    }
+    // public function testDeleteCategoria()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/categorias')
+    //            ->waitForText('Nova Categoria Dusk', 40) // Espera a categoria editada
+    //             ->click('button.ms-2.btn.btn-danger') // Clique no botão "Excluir"
+    //             ->waitUntilMissingText('Nova Categoria Dusk', 15) // Espera que a categoria desapareça
+    //             ->screenshot('categoria_excluida');
+    //     });
+    // }
 }
