f4fc362c787c176f4cb1e9f0154f3ae854626033
str1nng <juniormaceedo1@gmail.com>
2025-02-17 12:54:19 -0300

    Configura Jest e Babel para suportar módulos ES

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     presets: [
-      'react-app', // Preset usado pelo Create React App, que já inclui tudo o que você precisa
+      '@babel/preset-env', // Transpila código moderno para ES5
+      '@babel/preset-react' // Adiciona suporte ao React
     ],
     plugins: [
-      '@babel/plugin-proposal-private-methods',  // Se necessário
-      '@babel/plugin-proposal-private-property-in-object',  // Se necessário
-    ],
+      '@babel/plugin-proposal-private-methods', // Se necessário
+      '@babel/plugin-proposal-private-property-in-object' // Se necessário
+    ]
   };
-  
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
       "^.+\\.(js|jsx)$": "babel-jest"
     },
     transformIgnorePatterns: [
-        "/node_modules/(?!axios|other-library-to-transform)/"
-      ],
+      "/node_modules/(?!axios)/" // Transforma o axios e outras bibliotecas ES Modules
+    ],
     moduleNameMapper: {
       "\\.(css|scss)$": "identity-obj-proxy"
-    }
+    },
+    testEnvironment: "jsdom" // Adiciona suporte ao ambiente de navegador
   };
-  
