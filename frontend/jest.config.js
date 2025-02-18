b1ed06b882c3ce75fdb6e62656b5e2bfabc8b6ae
str1nng <juniormaceedo1@gmail.com>
2025-02-17 23:54:02 -0300

    Corrindo erros de compatibilidade

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest"
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!axios)/" // Transforma o axios e outras bibliotecas ES Modules
-    ],
-    moduleNameMapper: {
-      "\\.(css|scss)$": "identity-obj-proxy"
-    },
-    testEnvironment: "jsdom" // Adiciona suporte ao ambiente de navegador
-  };
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!axios|react-bootstrap).+\\.js$"
+  ],
+};
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Bem-vindo a Secretaria de Saúde', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Bem-vindo a Secretaria de Saúde/i);
   expect(linkElement).toBeInTheDocument();
 });
