abdb355e3c8ea32773d60eef587cf2cf3255df01
str1nng <juniormaceedo1@gmail.com>
2025-02-25 14:07:43 -0300

    Diretorios atualizados

--- codificacao/backend/tests/DuskTestCase.php
+++ codificacao/backend/tests/DuskTestCase.php
@@ -19,12 +19,12 @@ abstract class DuskTestCase extends BaseTestCase
     public static function prepare(): void
     {
         if (! static::runningInSail()) {
-            static::startChromeDriver([
+            static::startChromeDriver([ 
                 '--port=56709',
                 '--disable-dev-shm-usage',
                 '--no-sandbox', // Necessário para execução em containers, como no GitHub Actions
                 '--disable-gpu', // Necessário em alguns ambientes como CI
-                '--headless', // Certifique-se de que o Chrome está rodando em headless (opcional)
+                //'--headless', // Deixe o Chrome rodando em modo headless
                 '--user-data-dir=/tmp/dusk-user-data-dir', // Diretório único para os dados do usuário
             ]);
         }
@@ -39,15 +39,29 @@ abstract class DuskTestCase extends BaseTestCase
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
             '--disable-search-engine-choice-screen',
             '--disable-gpu',
-            '--headless', // Deixe o Chrome rodando em modo headless
+            //'--headless', // Deixe o Chrome rodando em modo headless
             '--user-data-dir=/tmp/dusk-user-data-dir', // Diretório único para os dados do usuário
         ])->all());
 
+        // Verificar se o ambiente está no GitHub Actions (CI) e definir a URL do WebDriver
+        $driverUrl = getenv('CI') ? 'http://host.docker.internal:56709' : ($_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:56709');
+
         return RemoteWebDriver::create(
-            $_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:56709',
+            $driverUrl, // Usar o WebDriver correto
             DesiredCapabilities::chrome()->setCapability(
                 ChromeOptions::CAPABILITY, $options
             )
         );
+    }
+
+    /**
+     * Define the base URL for your application.
+     *
+     * @return string
+     */
+    protected function baseUrl()
+    {
+        // Verificar se o ambiente está no GitHub Actions (CI) e definir a URL da aplicação
+        return getenv('CI') ? 'http://host.docker.internal:3000' : env('BASE_URL', 'http://localhost:3000');
     }
 }
